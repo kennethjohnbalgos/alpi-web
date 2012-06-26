37c96bda05742d784acfbca2f5125b7620f6fe87
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-06-26 13:52:14 +0800

    Homepage Interface

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlpiWeb::Application.config.secret_token = '7fdc4fd93ed6e74531b7cb3a06addbe4abefde896d1a95c30ae754cbb09b5f5ee2a9f237cccb19e72ed544239f51940e7690f88eb46085b27ec138bb5a1ac61f'
