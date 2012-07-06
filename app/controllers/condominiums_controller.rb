f43df385919dc3b365bf64bfb6439a7167304ffc
Kenneth John Balgos <kennethjohnbalgos@gmail.com>
2012-07-07 01:29:36 +0800

    Fix content arrangement

--- app/controllers/condominiums_controller.rb
+++ app/controllers/condominiums_controller.rb
@@ -1,9 +1,10 @@
 class CondominiumsController < ApplicationController
   def index
     @content = ContentCondo.find(1)
+    @content_items = ContentItemCondo.order(:title)
   end
 
   def view
-    @content = ContentItemCondo.find(params[:id])
+    @content = ContentItemCondo.find_by_title(params[:property])
   end
 end
--- app/controllers/residentials_controller.rb
+++ app/controllers/residentials_controller.rb
@@ -1,9 +1,10 @@
 class ResidentialsController < ApplicationController
   def index
     @content = ContentResidential.find(1)
+    @content_items = ContentItemResidential.order(:title)
   end
 
   def view
-    @content = ContentItemResidential.find(params[:id])
+    @content = ContentItemResidential.find_by_title(params[:property])
   end
 end
